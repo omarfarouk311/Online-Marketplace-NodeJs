8c0fa7d429f7a48cc7478cb8fa5d5c85b8026fb0
Omar <omarfarouk219@gmail.com>
2024-04-05 16:20:48 +0200

    switched to ejs and deleted unnecessary files

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const BodyParser = require('body-parser');
 const path = require('path');
-const admin_data = require('./routes/admin'), shop_router = require('./routes/shop');
+const admin_data = require('./routes/admin');
+const shop_router = require('./routes/shop');
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.set('views', 'views');
 
@@ -17,7 +18,7 @@ app.use('/admin', admin_data.routes);
 app.use(shop_router);
 
 app.use((req, res) => {
-    res.status(404).render('404', { pageTitle: 'page not found' })
+    res.status(404).render('404', { pageTitle: 'page not found', path: '/404' })
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-    this.products.push({ pageTitle: req.body.title });
+    this.products.push({ title: req.body.title });
     res.redirect('/');
 });
 
