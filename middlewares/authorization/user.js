b1512ffb770c461f76b40573230864a37734372c
Omar <omarfarouk219@gmail.com>
2024-06-14 04:58:54 +0300

    fix a bug in orders collection queries and add a check on product existence

--- middlewares/authorization/user.js
+++ middlewares/authorization/user.js
@@ -1,20 +1,37 @@
 const { getDb } = require('../../util/database');
+const { ObjectId } = require('mongodb');
 
 exports.authorizeProductModification = (req, res, next) => {
+    const db = getDb();
     const { productId } = (req.method === 'POST' ? req.body : req.params);
+
+    try {
+        const found = db.collection('products').findOne({ _id: ObjectId.createFromHexString(productId) });
+        if (!found) {
+            const err = new Error('Not found');
+            err.statusCode = 404;
+            throw err;
+        }
+    }
+    catch (err) {
+        return next(err);
+    }
+
     if (!req.user.products.find(prodId => prodId.toString() == productId)) {
         const err = new Error('Access denied');
         err.statusCode = 403;
         return next(err);
     }
+
     return next();
 }
 
 exports.authorizeInvoiceViewing = async (req, res, next) => {
     const db = getDb();
     const { orderId } = req.params;
+
     try {
-        let found = await db.collection('orders').findOne({ _id: orderId });
+        let found = await db.collection('orders').findOne({ _id: ObjectId.createFromHexString(orderId) });
         if (!found) {
             const err = new Error('Not Found');
             err.statusCode = 404;
@@ -23,7 +40,7 @@ exports.authorizeInvoiceViewing = async (req, res, next) => {
 
         found = await db.collection('users').findOne({
             _id: req.user._id,
-            orders: orderId
+            orders: ObjectId.createFromHexString(orderId)
         });
         if (!found) {
             const err = new Error('Access denied');
